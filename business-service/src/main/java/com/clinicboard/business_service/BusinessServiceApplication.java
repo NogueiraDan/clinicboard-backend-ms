3f9fc63a17065c434e155f55bf7c35fa600bc2ab
NogueiraDan <silvanogueira3460@gmail.com>
2025-08-08 15:24:08 -0300

    refactor: remover repositórios JPA obsoletos e adicionar configurações de escaneamento de entidades

--- business-service/src/main/java/com/clinicboard/business_service/BusinessServiceApplication.java
+++ business-service/src/main/java/com/clinicboard/business_service/BusinessServiceApplication.java
@@ -2,9 +2,11 @@ package com.clinicboard.business_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
 @EnableRabbit
@@ -12,6 +14,8 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 @EnableJpaAuditing
 @EnableFeignClients
 @SpringBootApplication
+@EntityScan(basePackages = "com.clinicboard.business_service.infrastructure.persistence.entity")
+@EnableJpaRepositories(basePackages = "com.clinicboard.business_service.infrastructure.persistence")
 public class BusinessServiceApplication {
 
 	public static void main(String[] args) {
